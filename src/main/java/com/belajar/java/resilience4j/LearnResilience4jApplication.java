b703012eb99ae6131b7af189a08a6c6e738da9cd
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-06-25 04:55:14 +0700

    first

---
+++ src/main/java/com/belajar/java/resilience4j/LearnResilience4jApplication.java
@@ -0,0 +1,13 @@
+package com.belajar.java.resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnResilience4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnResilience4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belajar/java/resilience4j/LearnResilience4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belajar.java.resilience4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnResilience4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
