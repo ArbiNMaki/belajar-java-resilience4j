a68e8fae683d502a9e44cc2ef4e766f4d93db0a4
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-06-25 15:26:30 +0700

    Rate Limiter Config

--- src/test/java/com/belajar/java/resilience4j/RateLimiterTest.java
+++ src/test/java/com/belajar/java/resilience4j/RateLimiterTest.java
@@ -1,9 +1,11 @@
 package com.belajar.java.resilience4j;
 
 import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
@@ -14,6 +16,25 @@ public class RateLimiterTest {
     @Test
     void testRateLimiter() {
         RateLimiter limiter = RateLimiter.ofDefaults("ak47");
+
+        for (int i = 0; i < 10_000; i++) {
+            Runnable runnable = RateLimiter.decorateRunnable(limiter, () -> {
+                long result = counter.incrementAndGet();
+                log.info("Result : {}", result);
+            });
+
+            runnable.run();
+        }
+    }
+
+    @Test
+    void testRateLimiterConfig() {
+        RateLimiterConfig rateLimiterConfig = RateLimiterConfig.custom()
+                .limitForPeriod(100)
+                .limitRefreshPeriod(Duration.ofMinutes(1))
+                .timeoutDuration(Duration.ofSeconds(2))
+                .build();
+        RateLimiter limiter = RateLimiter.of("ak47", rateLimiterConfig);
 
         for (int i = 0; i < 10_000; i++) {
             Runnable runnable = RateLimiter.decorateRunnable(limiter, () -> {
