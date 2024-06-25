61e610cc473e6afd058a4ab07bad83e6c88cf55b
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-06-26 03:42:42 +0700

    Event Publisher

---
+++ src/test/java/com/belajar/java/resilience4j/EventPublisherTest.java
@@ -0,0 +1,31 @@
+package com.belajar.java.resilience4j;
+
+import io.github.resilience4j.retry.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Supplier;
+
+@Slf4j
+public class EventPublisherTest {
+
+    private String hello() {
+        throw new IllegalArgumentException("Ups");
+    }
+
+    @Test
+    void testEventPublisher() {
+        Retry retry = Retry.ofDefaults("ak47");
+        retry.getEventPublisher().onRetry(_ -> log.info("Try to Retry"));
+
+        try {
+            Supplier<String> supplier = Retry.decorateSupplier(retry, this::hello);
+            supplier.get();
+        } catch (Exception e) {
+            System.out.println(retry.getMetrics().getNumberOfFailedCallsWithRetryAttempt());
+            System.out.println(retry.getMetrics().getNumberOfFailedCallsWithoutRetryAttempt());
+            System.out.println(retry.getMetrics().getNumberOfSuccessfulCallsWithRetryAttempt());
+            System.out.println(retry.getMetrics().getNumberOfSuccessfulCallsWithoutRetryAttempt());
+        }
+    }
+}
