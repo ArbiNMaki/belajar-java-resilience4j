741a3973f577a21f2095989a63c719dec3c97e48
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-06-26 02:50:30 +0700

    Circuit Breaker Config

--- src/test/java/com/belajar/java/resilience4j/CircuitBreakerTest.java
+++ src/test/java/com/belajar/java/resilience4j/CircuitBreakerTest.java
@@ -1,6 +1,7 @@
 package com.belajar.java.resilience4j;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +18,26 @@ public class CircuitBreakerTest {
         CircuitBreaker circuitBreaker = CircuitBreaker.ofDefaults("ak47");
 
         for (int i = 0; i < 200; i++) {
+            try {
+                Runnable runnable = CircuitBreaker.decorateRunnable(circuitBreaker, this::callMe);
+                runnable.run();
+            } catch (Exception e) {
+                log.error("Ups {}", e.getMessage());
+            }
+        }
+    }
+
+    @Test
+    void testCircuitBreakerConfig() {
+        CircuitBreakerConfig config = CircuitBreakerConfig.custom()
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                .failureRateThreshold(10f)
+                .slidingWindowSize(10)
+                .minimumNumberOfCalls(10)
+                .build();
+        CircuitBreaker circuitBreaker = CircuitBreaker.of("ak47", config);
+
+        for (int i = 0; i < 100; i++) {
             try {
                 Runnable runnable = CircuitBreaker.decorateRunnable(circuitBreaker, this::callMe);
                 runnable.run();
