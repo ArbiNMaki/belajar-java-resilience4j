af8f3a5b5ac32afd263bf707f12a55ea6beaad82
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-06-25 15:32:54 +0700

    Rate Limiter Registry

--- src/test/java/com/belajar/java/resilience4j/RateLimiterTest.java
+++ src/test/java/com/belajar/java/resilience4j/RateLimiterTest.java
@@ -2,6 +2,7 @@ package com.belajar.java.resilience4j;
 
 import io.github.resilience4j.ratelimiter.RateLimiter;
 import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -35,6 +36,28 @@ public class RateLimiterTest {
                 .timeoutDuration(Duration.ofSeconds(2))
                 .build();
         RateLimiter limiter = RateLimiter.of("ak47", rateLimiterConfig);
+
+        for (int i = 0; i < 10_000; i++) {
+            Runnable runnable = RateLimiter.decorateRunnable(limiter, () -> {
+                long result = counter.incrementAndGet();
+                log.info("Result : {}", result);
+            });
+
+            runnable.run();
+        }
+    }
+
+    @Test
+    void testRateLimiterRegistry() {
+        RateLimiterConfig config = RateLimiterConfig.custom()
+                .limitRefreshPeriod(Duration.ofMinutes(1))
+                .limitForPeriod(100)
+                .build();
+
+        RateLimiterRegistry registry = RateLimiterRegistry.ofDefaults();
+        registry.addConfiguration("config", config);
+
+        RateLimiter limiter = registry.rateLimiter("ak47", "config");
 
         for (int i = 0; i < 10_000; i++) {
             Runnable runnable = RateLimiter.decorateRunnable(limiter, () -> {
